9f956f01b9a60e6a80ab6d908e63dfba744bef72
github username <github email address>
2019-02-20 18:53:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+@@all = []
+
+attr_reader :name
+
+def initialize (name)
+  @name = name
+  @@all << self
+end
+
+def self.clear_all
+  @@all = []
+end
+
+def self.all
+  puts @@all
+end
+
+def self.all
+  puts @@all.map {|p| p.name}
+end
+
+end
